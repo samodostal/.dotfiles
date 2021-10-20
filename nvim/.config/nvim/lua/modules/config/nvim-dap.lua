72a5ab6307a989102cf4d85376a860393a5d21e5
samodostal <samuel.dostal.sk@gmail.com>
2021-10-20 20:20:12 +0200

    Add cpp debug (not worknig on M1 rn)

--- nvim/.config/nvim/lua/modules/config/nvim-dap.lua
+++ nvim/.config/nvim/lua/modules/config/nvim-dap.lua
@@ -12,6 +12,11 @@ return function()
       args = {os.getenv("HOME") .. "/.config/nvim/after/dap-adapters/vscode-chrome-debug/out/src/chromeDebug.js"}
   }
 
+  dap.adapters.cppdbg = {
+    type = 'executable',
+    command = '/Users/samueldostal/.dotfiles/nvim/.config/nvim/after/dap-adapters/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+  }
+
   dap.configurations.typescript = {
     {
         type = "chrome",
@@ -31,10 +36,37 @@ return function()
         request = "attach",
         program = "${file}",
         cwd = vim.fn.getcwd(),
+        miDebuggerPath = '/usr/local/bin/lldb',
         sourceMaps = true,
         protocol = "inspector",
         port = 9222,
         webRoot = "${workspaceFolder}"
     }
   }
+
+  dap.configurations.cpp = {
+    {
+      name = "Launch file",
+      type = "cppdbg",
+      request = "launch",
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopOnEntry = true,
+    },
+    {
+      name = 'Attach to gdbserver :1234',
+      type = 'cppdbg',
+      request = 'launch',
+      MIMode = 'gdb',
+      miDebuggerServerAddress = 'localhost:1234',
+      miDebuggerPath = '/usr/local/bin/lldb',
+      cwd = '${workspaceFolder}',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+    },
+  }
+
 end
