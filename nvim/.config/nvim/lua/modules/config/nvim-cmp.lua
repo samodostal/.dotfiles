e2eb8e8b5c424708ef13456047bd250e6e41e091
samodostal <samuel.dostal.sk@gmail.com>
2021-11-05 14:41:12 +0100

    Remove tab mapping for nvim-cmp
    It conflicted with Github Copilot

--- nvim/.config/nvim/lua/modules/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/modules/config/nvim-cmp.lua
@@ -2,11 +2,6 @@ return function()
   local cmp = require('cmp')
   local lspkind = require('lspkind')
 
-  local check_back_space = function()
-    local col = vim.fn.col '.' - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
-  end
-
   local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
   end
@@ -29,33 +24,7 @@ return function()
     mapping = {
       ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
-      ['<C-s>'] = cmp.mapping.complete(),
-      ['<Tab>'] = cmp.mapping(function(fallback)
-        if vim.fn.pumvisible() == 1 then
-          vim.fn.feedkeys(t '<C-n>', 'n')
-        elseif vim.fn.call('vsnip#available', { 1 }) == 1 then
-          vim.fn.feedkeys(t '<Plug>(vsnip-expand-or-jump)', '')
-        elseif check_back_space() then
-          vim.fn.feedkeys(t '<Tab>', 'n')
-        else
-          fallback()
-        end
-      end, {
-        'i',
-        's',
-      }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if vim.fn.pumvisible() == 1 then
-          vim.fn.feedkeys(t '<C-p>', 'n')
-        elseif vim.fn.call('vsnip#jumpable', { -1 }) == 1 then
-          vim.fn.feedkeys(t '<Plug>(vsnip-jump-prev)', '')
-        else
-          fallback()
-        end
-      end, {
-        'i',
-        's',
-      }),
+      ['<C-s>'] = cmp.mapping.complete()
     },
     sources = {
       -- { name = 'cmp_tabnine' },
