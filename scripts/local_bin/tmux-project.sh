dfb21388b4317bf4ea2d866881ccefcd7ee9c9fe
samodostal <samuel.dostal.sk@gmail.com>
2021-11-05 14:48:01 +0100

    Add eclipse workspace path to tmux-project.sh

--- scripts/local_bin/tmux-project.sh
+++ scripts/local_bin/tmux-project.sh
@@ -12,6 +12,7 @@ else
             ls -d ~/Projects/nvim_plugins/* | xargs -n 1 
             ls -d ~/Projects/smart_trial/* | xargs -n 1 
             ls -d ~/Projects/university/* | xargs -n 1 
+            ls -d ~/Projects/university/eclipse/*| xargs -n 1 
         ) | cut -c 30- | fzf --reverse
     )
 fi
