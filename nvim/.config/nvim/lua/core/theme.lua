ae0c90927356fb096cfce9fd684daa32cfd2d35c
samodostal <samuel.dostal.sk@gmail.com>
2022-07-19 20:42:35 +0200

    feat: gruvbox contrast update

--- nvim/.config/nvim/lua/core/theme.lua
+++ nvim/.config/nvim/lua/core/theme.lua
@@ -3,6 +3,7 @@ local colorscheme = 'gruvbox-material'
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_transparent_background = 1
 vim.g.gruvbox_material_background = 'soft'
+vim.g.gruvbox_material_foreground = 'mix'
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_ui_contrast = 'high'
