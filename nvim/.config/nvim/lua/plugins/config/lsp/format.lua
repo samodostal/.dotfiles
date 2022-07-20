f6146a8efd682b949e7b614040295ec59fabb85d
samodostal <samuel.dostal.sk@gmail.com>
2022-07-20 20:07:10 +0200

    feat: format js and ts files

--- nvim/.config/nvim/lua/plugins/config/lsp/format.lua
+++ nvim/.config/nvim/lua/plugins/config/lsp/format.lua
@@ -1,10 +1,10 @@
-local prettier_eslint_d = { formatCommand = 'prettier-eslint_d --write ${INPUT}', formatStdin = true }
+local prettier = { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
 local stylua = { formatCommand = 'stylua -s -', formatStdin = true }
 
 return {
 	lua = { stylua },
-	javascript = { prettier_eslint_d },
-	typescript = { prettier_eslint_d },
-	javascriptreact = { prettier_eslint_d },
-	typescriptreact = { prettier_eslint_d },
+	javascript = { prettier },
+	typescript = { prettier },
+	javascriptreact = { prettier },
+	typescriptreact = { prettier },
 }
