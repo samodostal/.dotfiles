37dc07fcc152a4131cf1855daf5b5939ec7cbce4
samodostal <samuel.dostal.sk@gmail.com>
2023-03-16 09:41:39 +0100

    feat: lazygit same neovim instance update

--- nvim/.config/nvim/lua/core/scripts/lazygit-open-file.lua
+++ nvim/.config/nvim/lua/core/scripts/lazygit-open-file.lua
@@ -1,5 +1,8 @@
-return function(filename, line_number_unparsed)
-	local line_number = tonumber(line_number_unparsed) or 1
+return function(filename, line_number)
+	line_number = tonumber(line_number) or 1
+
+	-- local utils = require 'core.utils'
+	-- utils.log(filename .. ':' .. line_number)
 
 	vim.api.nvim_win_close(0, true)
 	vim.api.nvim_command('edit +' .. line_number.. " " .. filename)
