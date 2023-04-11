3b528ed55ec46bfe2bc4c90a5dd896f8deadd7d9
samodostal <samuel.dostal.sk@gmail.com>
2023-04-11 14:03:50 +0200

    feat: help files vertical split

--- nvim/.config/nvim/lua/core/scripts/bufenter.lua
+++ nvim/.config/nvim/lua/core/scripts/bufenter.lua
@@ -1,5 +1,15 @@
-local banned_buf_names = { 'NvimTree', 'aerial' }
+-- local banned_buf_names = { 'NvimTree', 'aerial' }
 
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-	callback = function() end,
+-- vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+-- 	callback = function() end,
+-- })
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+	group = vim.api.nvim_create_augroup('help_window_right', {}),
+	pattern = { '*.txt' },
+	callback = function()
+		if vim.o.filetype == 'help' then
+			vim.cmd.wincmd 'L'
+		end
+	end,
 })
