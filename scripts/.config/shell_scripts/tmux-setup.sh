8d01a3b65e97cbeb48b077d31d67e75863aa5953
samodostal <samuel.dostal.sk@gmail.com>
2025-02-03 15:38:33 +0100

    Add tmux for now, wezterm is incomplete

---
+++ scripts/.config/shell_scripts/tmux-setup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Creates and configures tmux sessions
+
+# HOME
+tmux has-session -t home 2>/dev/null
+if [ $? -ne 0 ]; then
+    tmux new-session -ds home -c ~/
+    tmux send-keys -t home 'clear && neofetch' 'C-m'
+fi
+
+# DOTFILES
+tmux has-session -t dotfiles 2>/dev/null
+if [ $? -ne 0 ]; then
+    tmux new-session -ds dotfiles -c ~/.dotfiles
+    tmux send-keys -t dotfiles 'nvim' 'C-m'
+fi
+
+# NOTES
+tmux has-session -t notes 2>/dev/null
+if [ $? -ne 0 ]; then
+    tmux new-session -ds notes -c ~/Notes/muni/semester_5
+    tmux send-keys -t notes 'nvim' 'C-m'
+fi
+
+# GHCI
+tmux has-session -t ghci 2>/dev/null
+if [ $? -ne 0 ]; then
+    tmux new-session -ds ghci
+    tmux send-keys -t ghci 'clear && ghci' 'C-m'
+fi
+
+# DOCKER
+tmux has-session -t docker 2>/dev/null
+if [ $? -ne 0 ]; then
+    tmux new-session -ds docker
+    tmux send-keys -t docker 'clear && lazydocker' 'C-m'
+fi
+
+# BTOP
+tmux has-session -t btop 2>/dev/null
+if [ $? -ne 0 ]; then
+    tmux new-session -ds btop
+    tmux send-keys -t btop 'clear && btop' 'C-m'
+fi
+
+tmux attach-session -t home
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,61 +1,74 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.default_domain = "local"
 
 config.term = "xterm-256color"
-config.color_scheme = 'GruvboxDarkHard'
-config.window_background_opacity = 0.9
+config.color_scheme = "GruvboxDarkHard"
+-- config.window_background_opacity = 0.9
+config.window_background_opacity = 1.0
 config.window_padding = { left = 10, right = 10, top = 10, bottom = 10 }
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.font_size = 10.0
 
 config.font = wezterm.font({
-  family = 'JetBrains Mono',
+	family = "JetBrains Mono",
 })
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-
-config.enable_tab_bar = true
-
--- Keymaps
-local action = wezterm.action
-
-config.leader = { key = "u", mods = "CTRL", timeout_milliseconds = 2000 }
-
-local keys = {
-    { "s", action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { "v", action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { "h", action.ActivatePaneDirection("Left") },
-    { "l", action.ActivatePaneDirection("Right") },
-    { "j", action.ActivatePaneDirection("Down") },
-    { "k", action.ActivatePaneDirection("Up") },
-    { "L", action.AdjustPaneSize({ "Right", 25 }) },
-    { "H", action.AdjustPaneSize({ "Left", 25 }) },
-    { "c", action.SpawnTab("CurrentPaneDomain") },
-    { "h", action.ActivateTab(0) },
-    { "m", action.ActivateTab(1) },
-    { "w", action.ActivateTab(2) },
-    { "v", action.ActivateTab(3) },
-    { "z", action.ActivateTab(4) },
-    { "b", action.ShowLauncherArgs({ flags = "FUZZY|DOMAINS" }) }
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+	},
 }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
-config.keys = {}
-
-for i, v in ipairs(keys) do
-    table.insert(config.keys, {
-        mods = "LEADER",
-        key = v[1],
-	action = v[2]
-    })
-end
-
-wezterm.on("update-status", function(window, pane)
-  local domain = pane:get_domain_name()
-  window:set_left_status(wezterm.format({
-    { Text = " [" .. domain .. "] " },
-  }))
-end)
-
+config.enable_tab_bar = false
+--
+-- -- Keymaps
+-- local action = wezterm.action
+--
+-- config.leader = { key = "u", mods = "CTRL", timeout_milliseconds = 2000 }
+--
+-- local keys = {
+--     { "s", action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+--     { "v", action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+--     { "h", action.ActivatePaneDirection("Left") },
+--     { "l", action.ActivatePaneDirection("Right") },
+--     { "j", action.ActivatePaneDirection("Down") },
+--     { "k", action.ActivatePaneDirection("Up") },
+--     { "L", action.AdjustPaneSize({ "Right", 25 }) },
+--     { "H", action.AdjustPaneSize({ "Left", 25 }) },
+--     { "c", action.SpawnTab("CurrentPaneDomain") },
+--     { "h", action.ActivateTab(0) },
+--     { "m", action.ActivateTab(1) },
+--     { "w", action.ActivateTab(2) },
+--     { "v", action.ActivateTab(3) },
+--     { "z", action.ActivateTab(4) },
+--     { "b", action.ShowLauncherArgs({ flags = "FUZZY|DOMAINS" }) }
+-- }
+--
+-- config.keys = {}
+--
+-- for i, v in ipairs(keys) do
+--     table.insert(config.keys, {
+--         mods = "LEADER",
+--         key = v[1],
+-- 	action = v[2]
+--     })
+-- end
+--
+-- wezterm.on("update-status", function(window, pane)
+--   local domain = pane:get_domain_name()
+--   window:set_left_status(wezterm.format({
+--     { Text = " [" .. domain .. "] " },
+--   }))
+-- end)
+--
 return config
