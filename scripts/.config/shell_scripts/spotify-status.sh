0890ea2dea4498b41e5c3e33d3ccda4aabf6d0e1
samodostal <samuel.dostal.sk@gmail.com>
2025-02-18 10:02:11 +0100

    Add spotify status module to waybar

---
+++ scripts/.config/shell_scripts/spotify-status.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+status=$(playerctl status 2>/dev/null)
+metadata=$(playerctl metadata --format '{{artist}} - {{title}}' 2>/dev/null)
+
+if [ "$status" = "Playing" ]; then
+    icon=" "
+elif [ "$status" = "Paused" ]; then
+    icon=" "
+else
+    icon=" "
+    metadata="No music playing"
+fi
+
+echo "$icon $metadata"
