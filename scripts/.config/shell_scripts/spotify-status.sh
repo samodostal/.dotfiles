e65d662e2956d2b2868d095d095b0c910b03a5c9
samodostal <samuel.dostal.sk@gmail.com>
2025-09-22 08:00:31 +0200

    Upgrade spotify-status shell script to truncate long strings

--- scripts/.config/shell_scripts/spotify-status.sh
+++ scripts/.config/shell_scripts/spotify-status.sh
@@ -1,15 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+readonly MAX_LENGTH=32
 
 status=$(playerctl status 2>/dev/null)
 metadata=$(playerctl metadata --format '{{artist}} - {{title}}' 2>/dev/null)
 
+if [ ${#metadata} -gt $MAX_LENGTH ]; then
+	trimmed="${metadata:0:$MAX_LENGTH}"
+	trimmed=$(echo -n "$trimmed" | sed 's/[[:space:]]*$//')
+	metadata="${trimmed}..."
+fi
+
 if [ "$status" = "Playing" ]; then
-    icon=""
+	icon=""
 elif [ "$status" = "Paused" ]; then
-    icon=""
+	icon=""
 else
-    icon=""
-    metadata="No music playing"
+	icon=""
+	metadata="No music playing"
 fi
 
 echo "$icon $metadata"
